348ba64e796f8003347693791237b832b58b59d7
Alex Villarreal <alex.villarreal@definityfirst.com>
2017-12-09 20:30:23 -0600

    Day 2 puzzle 2

--- day2.py
+++ day2.py
@@ -1,21 +1,14 @@
 ''' Day 2 from Advent of Code 2017
 '''
 
-def calculate_total(data, step):
-    '''Calculates the sum of the digits that are equal to their 'sibling',
-    which is <step> positions ahead in the circular list''' 
-    total = 0
-    length = len(input_data)
-    for i in range(0, length - 1):
-        if data[i] == data[(i+step) % length]:
-            total += int(data[i])
-    return total
-
 import utils
 input_data = utils.read_file(2).read()
 lines = [list(map(lambda x: int(x), line.split('\t'))) for line in input_data.split('\n')]
 
-# print(lines)
+# Puzzle 1
 line_checksums = [max(line)-min(line) for line in lines]
-# print(line_checksums)
 print(sum(line_checksums))
+
+# Puzzle 2
+line_checksums_2 = [n//m for line in lines for n in line for m in line if n > m and n % m == 0]
+print(sum(line_checksums_2))
