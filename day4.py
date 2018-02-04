9ec72e541a4d76f33db2737c7757dbc148e53815
Alex Villarreal <alex.villarreal@definityfirst.com>
2018-02-04 00:36:43 -0600

    Day 4 puzzle 2

--- day4.py
+++ day4.py
@@ -5,4 +5,10 @@ def is_valid(passphrase):
     words = passphrase.split(' ')
     return len(words) == len(set(words))
 
+def is_valid_stronger(passphrase):
+    words = passphrase.split(' ')
+    words_with_sorted_letters = [''.join(sorted(w)) for w in words]
+    return len(words_with_sorted_letters) == len(set(words_with_sorted_letters))
+
 print(len([passphrase for passphrase in input_data if is_valid(passphrase)]))
+print(len([passphrase for passphrase in input_data if is_valid_stronger(passphrase)]))
